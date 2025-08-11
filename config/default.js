df3531860a1c353387dcd9dc58f296675faaac57
sohandsomejie <3080955413@qq.com>
2025-08-11 22:57:55 +0800

    fix: 修正数据库主机地址为127.0.0.1

--- config/default.js
+++ config/default.js
@@ -2,7 +2,7 @@ module.exports = {
   port: process.env.PORT || 3000,
   env: process.env.NODE_ENV || 'development',
   mysql: {
-    host: process.env.DB_HOST || '192.168.31.2',
+    host: process.env.DB_HOST || '127.0.0.1',
     port: Number(process.env.DB_PORT || 3306),
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '1233300',
